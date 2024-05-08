1f9da325b7def897bc42b9c5713f7a4536fa1638
Veronica <vero.dm@yandex.ru>
2024-05-09 00:03:03 +0300

    header

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+ // Menu
+
+ function openModal() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-700px";
+}
