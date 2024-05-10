8b798279166dd51f8880074980e22aad1cd5cb9b
Veronica <vero.dm@yandex.ru>
2024-05-11 00:12:46 +0300

    timetable

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,10 @@
 
  function openModal() {
     document.getElementById("modal").style.top = "0px";
+    //document.getElementById("modal").style.display = "block";
 }
 
 function closeModal() {
     document.getElementById("modal").style.top = "-700px";
+    //document.getElementById("modal").style.display = "none";
 }
